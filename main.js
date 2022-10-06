7506a4b38cec508f6474e8a860ae2e4839dd40ec
spoojler <inz.mateuszwojcik@gmail.com>
2022-10-06 02:11:57 +0200

    Header with burger menu

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+burger.addEventListener('click', (event) => {
+  if (burger.classList.contains('active')) {
+    burger.classList.remove('active');
+    menu.classList.remove('active');
+  } else {
+    burger.classList.add('active');
+    menu.classList.add('active');
+  }
+});
